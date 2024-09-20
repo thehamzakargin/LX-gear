ea2d4d72667cefe7f0a69580250ed3a7ca3509cb
thehamzakargin <hamzakargin@outlook.com>
2024-09-20 10:32:09 +0300

    Remove injected with rxjs

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -33,4 +33,10 @@ export class UserPlacesComponent implements OnInit {
       subscription.unsubscribe();
     });
   }
+  onRemovePlace(place: Place) {
+    const subscription = this.placesService.removeUserPlace(place).subscribe();
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
